80d6871bd6e96483a8e0886bc6e6f064d6f879bd
Steve Goguen <sgoguen@gmail.com>
2019-03-01 19:28:12 -0500

    Saved Tree

--- dotnetLibsInFSharp/src/Trees/Library.fs
+++ dotnetLibsInFSharp/src/Trees/Library.fs
@@ -4,7 +4,11 @@ type ITree<'a> =
     abstract member Value: 'a
     abstract member Children: seq<ITree<'a>>
 
-
+module Tree = 
+    let create value = 
+    { new ITree<_> with
+        member this.Value = value 
+        member this.Children = Seq.empty }
 
 
 
